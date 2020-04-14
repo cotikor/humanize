68de43bd6086d2b48bc1e0f923238aea0797059f
Chaya Otikor <c.otikor@gmail.com>
2020-04-14 16:12:35 -0600

    basic setup of youtube playlist

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,26 @@
 <template>
-  <div class="container">
-    <Carousel />
-    <Playlist />
-  </div>
+	<div class="container">
+		<Carousel />
+		<Playlist />
+	</div>
 </template>
 
 <script>
-import Carousel from './Carousel'
-import Playlist from './Playlist'
-
+import Carousel from "./Carousel";
+import Playlist from "./Playlist";
 
 export default {
-  name: 'Home',
-  components: {
-      Carousel,
-      Playlist
-  }
-}
+	name: "Home",
+	components: {
+		Carousel,
+		Playlist,
+	},
+};
 </script>
 
 <style scoped>
 .container {
-    display: flex;
-    flex-flow: column nowrap;
+	display: flex;
+	flex-flow: column nowrap;
 }
 </style>
--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -1,42 +1,59 @@
 <template>
-  <div class="container">
-    <h1>Latest Talks</h1>
-    {{ playlist }}
-  </div>
+	<div class="container">
+		<h1>Latest Talks</h1>
+
+		<a
+			v-for="item in playlist"
+			:key="item.id"
+			:href="`https://youtu.be/${item.snippet.resourceId.videoId}`"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<section class="video-container">
+				<img :src="item.snippet.thumbnails.default.url" />
+				<h1>{{ item.snippet.title }}</h1>
+			</section>
+		</a>
+	</div>
 </template>
 
 <script>
 import axios from "axios";
 export default {
-  name: "Playlist",
-  data() {
-    return {
-      playlist: null
-    };
-  },
-  mounted() {
-    axios({
-      method: "get",
-      url: "https://www.googleapis.com/youtube/v3/playlistItems",
-      params: {
-        part: "snippet",
-        playlistId: process.env.VUE_APP_PLAYLIST_ID,
-        key: process.env.VUE_APP_GOOGLE_API_KEY
-      }
-    })
-      .then(res => {
-        console.log("res", res);
-        this.playlist = res;
-      })
-      .catch(err => {
-        console.log({ err }, process.env);
-      });
-  }
+	name: "Playlist",
+	data() {
+		return {
+			playlist: null,
+		};
+	},
+	mounted() {
+		axios({
+			method: "get",
+			url: "https://www.googleapis.com/youtube/v3/playlistItems",
+			params: {
+				part: "snippet",
+				playlistId: process.env.VUE_APP_PLAYLIST_ID,
+				key: process.env.VUE_APP_GOOGLE_API_KEY,
+				maxResults: 20,
+			},
+		})
+			.then((res) => {
+				console.log("res", res.data.items);
+				this.playlist = res.data.items;
+			})
+			.catch((err) => {
+				console.log({ err });
+			});
+	},
 };
 </script>
 
 <style scoped>
 .container {
-  border: 1px solid red;
+	border: 1px solid red;
+	overflow: scroll;
+}
+.video-container {
+	border: 1px solid black;
 }
 </style>
